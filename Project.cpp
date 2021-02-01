50d7e550c59148ac6ffc39dc4413458fb534bcf8
Zack Hawkins <zackh019@gmail.com>
2021-02-01 04:47:22 +0000

    Comments

--- Project.cpp
+++ Project.cpp
@@ -10,7 +10,8 @@
 using namespace std;
 using namespace glm;
 
-//width and height of the window ( Aspect ratio 16:9 )
+
+//Width and height of the window ( Aspect ratio 16:9 )
 const int SRC_width = 16 * 50;
 const int SRC_height = 9 * 50;
 
@@ -20,11 +21,8 @@ float lastX = SRC_width / 2.0f;
 float lastY = SRC_height / 2.0f;
 bool firstMouse = true;
 
-int onMouse = 0;
 
 vec3 spawnpoint = vec3(0.0f, 0.15f, -14.8f);
-
-
 
 
 float deltaTime = 0.0f;	// time between current frame and last frame
@@ -52,13 +50,12 @@ void spawnWallLeft(Shader& shaders);
 void spawnWallRight(Shader& shaders);
 void spawnCeiling(Shader &shaders);
 
-bool test = false;
 
 
 
 
-
-
+//  Main loop for the program, calls all of the required methods and the ones 
+//that are required to be inside of a while loop are
 int main(int argc, char** argv)
 {
 
@@ -105,7 +102,7 @@ int main(int argc, char** argv)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
 
-        
+        //Spawns all of the walls into the scene
         spawnWallBack(shaders);
         spawnWallFront(shaders);
         spawnWallLeft(shaders);
@@ -113,10 +110,10 @@ int main(int argc, char** argv)
         spawnFloor(shaders);
         spawnCeiling(shaders);
 
-
+        //Spawns the starting crosshair
         spawnTarget(shaders, spawnpoint);
 
-
+        //Creates the crosshair
         crosshair1(hud);
         crosshair2(hud);
         crosshair3(hud);
@@ -124,7 +121,7 @@ int main(int argc, char** argv)
 
        
         
-
+        //gets the current matrix view for the camera
         glm::mat4 view = camera.GetViewMatrix();
         shaders.setMat4("view", view);
 
@@ -143,7 +140,12 @@ int main(int argc, char** argv)
     
 }
 
-
+//  These series of methods are used to create the crosshair that the player 
+//can use inside the scene. Each of the methods creates and draws one section
+//of the crosshair. All of them combined are needed to complete the crosshair.
+//In order to render it as a hud rather than a 3D object, different shaders are
+//used that are specifically for hud elements that are currently used and that 
+//can be added at a later date.
 void crosshair1(Shader& hud) {
 
     float vertices[] = {
@@ -304,18 +306,26 @@ void crosshair4(Shader& hud) {
 }
 
 
+//  These next 4 methods are related to the camera movement and player movement that 
+//I outsourced. Due to this I have a vague understanding of how they work. Scroll
+//callback is for the zoom function that is bound to the scroll wheel, it is an event
+//handler. Frame buffer size makes sure that the viewport matches the diamensions of
+//the window. Process input is where all of the inputs are collected and fed back to
+//the camera.h class. It checks to see if W,A,S,D or ESC have been pressed and acts
+//accordingly. W,A,S,D pass through to the camera class and ESC closes the window.
+//  Mouse_callback tracks the current where abouts of the player's mouse in the window
+//and sends the data back to the camera class, once there it updates the scene to match
+//the new view that they should be seeing.
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
 }
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
-
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -330,7 +340,6 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
-
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     if (firstMouse)
@@ -350,7 +359,15 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 }
 
 
-
+//  These 6 methods are used to create and render each of the walls for the scene. This is
+//done by using a shader to apply a colour to the vertices and in turn indices that are
+//created by each of these methods. The data for each is then collected and stored in 
+//buffers. Once the data has been stored, it then can be accessed for use in the mat4.
+//This is a much easier way to rotate, change the colour of and translate the object
+//once it has been created. This is also required to render the object in a 3D space.
+//  Once the model ahs been assigned, the rotation, scale and translation of the model
+//can be edited. Once this final changes have been made, the vertex array can be 
+//bound and the object drawn in the scene.
 void spawnWallBack(Shader& shade) {
 
     float vertices[] = {
@@ -667,12 +684,29 @@ void spawnFloor(Shader& shade) {
 }
 
 
+//This is a event handler to check if the mouse has been clicked (specifically the
+//left mouse button). If the button has been pressed and is pressed down, the 
+//method "FetchColour" is called.
 void MouseClick(GLFWwindow* window, int button, int action, int mods) {
     if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN) {
         FetchColour();
     }
 }
 
+//  This method is used to calculate the colour of the pixel that is currently
+//located in the middle of the window. This is done by halving the width and
+//height of the window by 2. The function read pixels is then called to 
+//calculate the colour that is currently being selected in the middle of 
+//window. Once the colour has been read, it then gets converted back into
+//and integer which gets used as an ID for the code to read. If the ID read
+//by the function matches that of the colour that I have specified as the 
+//colour of the target, it then follows through with the next section of code.
+//  This creates a new vector 3 for a spawnpoint of a target. Using the rand
+//function it gets new values for the x and y of the vector 3 within certain
+//bounds. Once it has these, along with a preset value for z (we never want
+//the z value to change) it sets this new vector 3 as the global vector 3 that
+//the spawnTarget method uses. It them calls the spawnTarget method to 
+//create/move the current target to its new, pseudo-randomly decided position.
 void FetchColour() {
 
     unsigned char data[4];
@@ -683,7 +717,6 @@ void FetchColour() {
         data[0] +
         data[1] * 256 +
         data[2] * 256 * 256;
-    //cout << pickedID << endl;
 
     if (getID == 10027059)  //If colour hit
     {
@@ -697,10 +730,17 @@ void FetchColour() {
 
         Shader shaders("media/triangles.vert", "media/triangles.frag");
         spawnTarget(shaders, spawnpoint);
-        cout << "HIT" << endl;
     }
 }
 
+//  This last method is used to create the targets that the player can "shoot" at.
+//This is done by using the same method as creating the walls before. We are
+//setting scale to 0.5 to make it more target like rather than a wall. The colour
+//for the target also has to be different than any other colour used in the scene
+//as not to confused the method that gets and checks the colour against the 
+//centre of the window. The major difference with this method is that the vector 3
+//is declared at the top of the program. This is so that we can change it in our
+//FetchColour method without causing any issues with it spawning.
 void spawnTarget(Shader& shade, vec3 spawn) {
 
     
